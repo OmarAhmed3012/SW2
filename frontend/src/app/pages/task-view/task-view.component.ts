41e7aa6229150bb0835e3094babb9d46558d762c
OmarAhmed3012 <omaraboshamaa@gmail.com>
2020-03-15 19:08:26 +0200

    adding task view

---
+++ frontend/src/app/pages/task-view/task-view.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/task.service';
+import { ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-task-view',
+  templateUrl: './task-view.component.html',
+  styleUrls: ['./task-view.component.scss']
+})
+export class TaskViewComponent implements OnInit {
+
+  lists: any[]
+  Tasks: any[]
+
+  constructor(private taskService: TaskService, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.taskService.getTasks(params.listId).subscribe((tasks: any[]) => {
+          this.Tasks = tasks
+        })
+      }
+    )
+    this.taskService.getLists().subscribe((lists: any[]) => {
+      this.lists = lists
+    })
+  }
+
+  
+
+}
