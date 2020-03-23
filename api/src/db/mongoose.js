a1b287451d35b77b029d2159e15411fb5a3a8a38
AbdelrahmanHashem468 <ahmedabdelrahman468@gmail.com>
2020-03-23 23:37:44 +0200

    Handling Routers and Models..Finishing Login and Logout

--- api/src/db/mongoose.js
+++ api/src/db/mongoose.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose')
 //const {MONGODB_URL} = require('../../config/constrains')  //process.env.MONGODB_URL
 /////
-mongoose.connect('mongodb+srv://taskapp:omarahmed3012@cluster0-irrbg.mongodb.net/task-manager-api?retryWrites=true', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
+mongoose.connect('mongodb://127.0.0.1:27017/TaskManager', {
+    useNewUrlParser: true
 })
-//
+
+
+// testing
+
+const url = 'mongodb://127.0.0.1:27017/TaskManager'
+
+const db = mongoose.connection
+db.once('open', _ => {
+  console.log('Database connected:', url)
+})
+
+db.on('error', err => {
+  console.error('connection error:', err)
+})
---
+++ api/src/index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+require('./db/mongoose')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+
+const app = express()
+const port = process.env.PORT || 3000
+//dfjn
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//     next();
+// });
+
+
+const bodyParser = require('body-parser');
+
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token, x-refresh-token, _id");
+
+    res.header(
+        'Access-Control-Expose-Headers',
+        'x-access-token, x-refresh-token'
+    );
+
+    next();
+});
+
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
