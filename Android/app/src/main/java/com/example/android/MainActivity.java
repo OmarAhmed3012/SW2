db895ca3a6d1fda1f18e7bc29d89f8736282061a
Karim <karimomakram11@gmail.com>
2020-03-27 01:25:31 +0200

    switch Activity

--- Android/app/src/main/java/com/example/android/MainActivity.java
+++ Android/app/src/main/java/com/example/android/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+    EditText email,password;
+    CheckBox checkBox;
+    ImageButton signin;
+    Button signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        email = (EditText)findViewById(R.id.email);
+        password = (EditText)findViewById(R.id.password);
+        password.setShowSoftInputOnFocus(true);
+        checkBox = (CheckBox)findViewById(R.id.checkbox);
+
+        signin =(ImageButton)findViewById(R.id.signin);
+
+        signup = (Button) findViewById(R.id.signup);
+
+        signin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Sign In Button Clicked",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,SignUp.class);
+                startActivity(i);
+            }
+        });
     }
 }
